cb4649bb31c38d25fa1563f04de35bf696d32c61
Lisette Pineda <lpineda@avvo.com>
2018-01-04 15:44:40 -0800

    eslint w/ airbnb working

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Abcdefg...</Text>
+    <Text>my name is...</Text>
+    <Text>lisy : ) </Text>
+  </View>
+);
+
+export default App;
